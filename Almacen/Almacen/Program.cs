efb4a8e5608a3b46c7d32bac30be81e9e3509690
Jazmincita13 <125412889+Jazmincita13@users.noreply.github.com>
2023-03-15 17:36:40 -0600

    Add files via upload

---
+++ Almacen/Almacen/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Almacen.Models;
+
+
+using (AlmacenContext db = new AlmacenContext())
+{
+    Producto producto = new Producto();
+
+    Console.WriteLine("Ingrese nombre del producto");
+    producto.Nombre = Console.ReadLine();
+
+    Console.WriteLine("");
+
+    Console.WriteLine("Ingrese descripcion del producto");
+    producto.Descripcion = Console.ReadLine();
+
+    Console.WriteLine("");
+
+    Console.WriteLine("Ingrese precio del producto");
+    producto.Precio = Convert.ToDecimal(Console.ReadLine());
+
+    Console.WriteLine("");
+
+    Console.WriteLine("Ingrese el Stock");
+    producto.Stock = Convert.ToInt32(Console.ReadLine());
+
+    db.Productos.Add(producto);
+    db.SaveChanges();
+
+
+
+}
+
